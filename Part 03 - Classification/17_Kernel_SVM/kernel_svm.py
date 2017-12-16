7387e37dbf2a04fe92bc6a73df2a7d305b205628
SLPeoples <33819909+SLPeoples@users.noreply.github.com>
2017-12-16 14:32:34 -0800

    kernels

--- Part 03 - Classification/17_Kernel_SVM/kernel_svm.py
+++ Part 03 - Classification/17_Kernel_SVM/kernel_svm.py
@@ -43,7 +43,7 @@ plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+                c = ListedColormap(('brown', 'green'))(i), label = j)
 plt.title('Kernel SVM (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
@@ -61,7 +61,7 @@ plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+                c = ListedColormap(('brown', 'green'))(i), label = j)
 plt.title('Kernel SVM (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
